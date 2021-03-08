d3b8ccccdad3e584494433cc868099c1199d8224
NailYS <nail.yusupov.08@bk.ru>
2021-03-08 22:18:19 +0400

    queue finished

--- src/main/java/com/example/MyQueue.java
+++ src/main/java/com/example/MyQueue.java
@@ -3,69 +3,70 @@ package com.example;
 import java.util.Scanner;
 
 public class MyQueue {
+    final static int Size = 100;
+    static int[] array = new int[Size];
+    static int size = 0;
+    static int start = 0;
+    static int end = 0;
 
-    static int maxSize = 100;
-    static int[] queArray;
-    static int front = 0;
-    static int nItems = 0;
-    static int rear = 0;
+    static void push(int value) {
 
-    public MyQueue() {
-        queArray = new int[maxSize];
+        if (end > Size - 1) {
+            end = 0;
+        }
+        array[end++] = value;
+        System.out.println("ok");
+        size++;
+    }
+
+    static void clear() {
+        start = 0;
+        end = 0;
+        size = 0;
+        System.out.println("ok");
+
+    }
+
+    static void front() {
+
+        if (start > Size - 1) {
+            System.out.println(array[0]);
+        } else {
+            System.out.println(array[start]);
+        }
+
+    }
+
+    static void size() {
+        System.out.println(size);
     }
 
 
-    static void insert(int j) {
-        if (rear != maxSize - 1)
-            rear = -1;
-        System.out.println(queArray[++rear] = j);
-        nItems++;
-    }
-
-
-    static long remove() {
-        long temp = queArray[front++];
-        if (front == maxSize)
-            front = 0;
-        nItems--;
-        return temp;
-    }
-
-
-    static long peekFront() {
-        return queArray[front];
-    }
-
-    static boolean isEmpty() {
-        return (nItems == 0);
-    }
-
-    static boolean isFull() {
-        return (nItems == maxSize);
-    }
-
-
-    static int size() {
-        return size();
+    static void pop() {
+        if (start > Size - 1)
+            start = 0;
+        System.out.println(array[start++]);
+        size--;
     }
 
     public static void main(String[] args) {
-//        myQueue.insert(3);
-//        myQueue.insert(5 );
-//        myQueue.insert(8);
-        MyQueue myQueue = new MyQueue();
         Scanner scan = new Scanner(System.in);
-        String command;
-        command = scan.next();
-        while (!command.equals("exit")) {
-            switch (command) {
+        String string;
+        int value;
+        string = scan.next();
+        while (!string.equals("exit")) {
+            switch (string) {
                 case "push": {
-                    int j = scan.nextInt();
-                    myQueue.insert(j);
+                    value = scan.nextInt();
+                    push(value);
                     break;
                 }
                 case "pop": {
-
+                    pop();
+                    break;
+                }
+                case "front": {
+                    front();
                     break;
                 }
                 case "size": {
@@ -73,21 +74,15 @@ public class MyQueue {
                     break;
                 }
                 case "clear": {
-
-                }
-                case "front": {
+                    clear();
                     break;
                 }
+
+
             }
-            command = scan.next();
-            for (int i = 0; i < queArray.length; i++) {
-                System.out.println(queArray[rear]);
-                break;
-            }
+            string = scan.next();
         }
         System.out.println("bye");
 
     }
 }
-
-
